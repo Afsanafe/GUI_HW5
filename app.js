44aa486b475e43d35cdf9d1c57af417f63fb20c7
Afsa Nafe <afsanafe1@gmail.com>
2025-11-29 10:23:07 -0500

    Updated box css and updated the boards items placement

--- app.js
+++ app.js
@@ -3,17 +3,17 @@
 $(document).ready(function() {
     // Make the cards movable
     $(".card").draggable({
-        revert: "invalid",
-        helper: "clone"
+        revert: "invalid", // automatically handles the animation of the item snapping back if the user drops it outside the target
+        helper: "clone"    // Leaves the original item in place until the drop is confirmed 
     });
 
     // Both target and origin boxes can accept the cards
-    $("#origin, #target").droppable({
+    $("#origin, #target").droppable({ // The comma acts like an 'and' operator
         accept: ".card",
         activeClass: "highlight",
         drop: function(event, ui) {
             var droppedItem = ui.draggable;
-            $(this).append(droppedItem);
+            $(this).append(droppedItem); // 
             droppedItem.css({top: 0, left: 0});
         }
     });
